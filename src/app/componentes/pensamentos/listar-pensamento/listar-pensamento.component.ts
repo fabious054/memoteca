20da498cde667c390ba23b29dabeb649d2476f9c
Fábio Abreu <fabio.abreu@robozz.com.br>
2024-10-23 17:39:26 -0300

    complete angular evolution

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -13,18 +14,21 @@ export class ListarPensamentoComponent {
   qtdAtual: number = 3;
   haMaisPensamentos: boolean = true;
   filtro: string = '';
+  favorito = false;
+  listaFavoritos: Pensamento[] = [];
+  titulo: string = 'Meu mural';
 
-  constructor(private service: PensamentoService ) { }
+  constructor(private service: PensamentoService, private router: Router ) { }
 
   ngOnInit() {
-    this.service.listar(this.qtdAtual,this.filtro).subscribe(pensamentos => {
+    this.service.listar(this.qtdAtual,this.filtro,this.favorito).subscribe(pensamentos => {
       this.listaPensamentos = pensamentos;
 
     });
   }
 
   carregarMais() {
-    this.service.listar(++this.qtdAtual,this.filtro).subscribe(pensamentos => {
+    this.service.listar(++this.qtdAtual,this.filtro,this.favorito).subscribe(pensamentos => {
       const idsPensamentos = this.listaPensamentos.map(p => p.id);
       const pensamentosNaoInseridos = pensamentos.filter(p => !idsPensamentos.includes(p.id));
       if (!pensamentosNaoInseridos.length) {
@@ -37,8 +41,27 @@ export class ListarPensamentoComponent {
 
   pesquisarPensamentos(){
     this.haMaisPensamentos = true;
-    this.service.listar(this.qtdAtual,this.filtro).subscribe(pensamentos => {
+    this.service.listar(this.qtdAtual,this.filtro,this.favorito).subscribe(pensamentos => {
       this.listaPensamentos = pensamentos;
     });
+  }
+
+  listarFavoritos(){
+    this.titulo = 'Meus Favoritos';
+    this.haMaisPensamentos = true;
+    this.favorito = !this.favorito;
+    this.service.listar(this.qtdAtual,this.filtro,this.favorito).subscribe(pensamentosFav => {
+      this.listaPensamentos = pensamentosFav;
+      this.listaFavoritos = pensamentosFav;
+    });
+  }
+
+  recarregarComponente(){
+    this.titulo = 'Meu mural';
+    this.favorito = false;
+    this.qtdAtual = 3;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([this.router.url]);
   }
 }
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -11,13 +11,17 @@ export class PensamentoService {
   private readonly API = "http://localhost:3000/pensamentos";
   constructor(private http: HttpClient) { }
 
-  listar(limit: number,filtro:string): Observable<Pensamento[]> {
+  listar(limit: number,filtro:string,favoritos:boolean): Observable<Pensamento[]> {
     const qtdItens = limit;
     const pagina = 1;
     let params = new HttpParams().set('_page', pagina).set('_limit', qtdItens);
 
     if (filtro.trim()) {
       params = params.set('conteudo', filtro);
+    }
+
+    if(favoritos){
+      params = params.set('favorito', favoritos);
     }
 
 
